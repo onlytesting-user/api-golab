c1b149ad38f82210b7d15ccedc115d0e2862564e
onlytesting-user <testando.sotestando001@gmail.com>
2025-09-30 13:24:49 -0300

    feat: implement GetProducts method for ProductRepository

--- repository/product_repository.go
+++ repository/product_repository.go
@@ -4,6 +4,9 @@ package repository
 
 import (
 	"database/sql"
+	"fmt"
+
+	"github.com/onlytesting-user/api-golab/model"
 )
 
 // ProductRepository provides methods to perform operations on products
@@ -18,4 +21,34 @@ func NewProductRepository(connection *sql.DB) ProductRepository {
 	return ProductRepository{
 		connection: connection,
 	}
+}
+
+func (pr *ProductRepository) GetProducts() ([]model.Product, error) {
+	query := "SELECT id, product_name, price FROM product"
+	rows, err := pr.connection.Query(query)
+	if err != nil {
+		fmt.Println(err)
+		return []model.Product{}, err
+	}
+
+	var productList []model.Product
+	var productObj model.Product
+
+	for rows.Next() {
+		err = rows.Scan(
+			&productObj.ID,
+			&productObj.Name,
+			&productObj.Price,
+		)
+		if err != nil {
+			fmt.Println(err)
+			return []model.Product{}, err
+		}
+
+		productList = append(productList, productObj)
+	}
+
+	rows.Close()
+
+	return productList, nil
 }
