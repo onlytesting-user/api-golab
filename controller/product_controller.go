3bdac8b5ddb57d9841bd46317168b371f4a8abf3
onlytesting-user <testando.sotestando001@gmail.com>
2025-10-01 09:13:49 -0300

    feat: add CreateProduct function in controller

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/onlytesting-user/api-golab/model"
 	"github.com/onlytesting-user/api-golab/usecase"
 )
 
@@ -21,9 +22,26 @@ func NewProductController(usecase usecase.ProductUsecase) productController {
 
 func (p *productController) GetProducts(ctx *gin.Context) {
 	products, err := p.productUseCase.GetProducts()
-	if (err != nil) {
+	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, err)
 	}
 
 	ctx.JSON(http.StatusOK, products)
+}
+
+func (p *productController) CreateProduct(ctx *gin.Context) {
+	var product model.Product
+	err := ctx.BindJSON(&product)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	insertedProduct, err := p.productUseCase.CreateProduct(product)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, insertedProduct)
 }
