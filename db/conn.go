7a0856441a81df51fa1e9ba38d5f7f70ee2db36e
onlytesting-user <testando.sotestando001@gmail.com>
2025-10-01 07:22:02 -0300

    refactor: load env vars with godotenv and .env support

--- db/conn.go
+++ db/conn.go
@@ -6,32 +6,40 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
 func ConnectDB() (*sql.DB, error) {
+	// Carrega variáveis do .env (não falha se o arquivo não existir)
+	_ = godotenv.Load()
+
 	port, err := strconv.Atoi(os.Getenv("DB_PORT"))
 	if err != nil {
 		return nil, fmt.Errorf("invalid DB_PORT: %w", err)
 	}
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
 		port,
 		os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("POSTGRES_DB"))
+		os.Getenv("POSTGRES_DB"),
+	)
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	err = db.Ping()
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	fmt.Println("Connected to: ", os.Getenv("DB_NAME"))
-
+	fmt.Println("Connected to:", os.Getenv("POSTGRES_DB"))
 	return db, nil
 }
--- go.mod
+++ go.mod
@@ -3,13 +3,18 @@ module github.com/onlytesting-user/api-golab
 go 1.25.1
 
 require (
+	github.com/gin-gonic/gin v1.11.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
+
+require (
 	github.com/bytedance/gopkg v0.1.3 // indirect
 	github.com/bytedance/sonic v1.14.1 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.10 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
-	github.com/gin-gonic/gin v1.11.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.27.0 // indirect
